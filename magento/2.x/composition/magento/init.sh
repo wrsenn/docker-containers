90fdeab5926bcd648ab1ec465d7e50888763ace7
Rob Senn <rob@blueacorn.com>
2017-02-20 21:48:51 -0500

    Updating containers

--- magento/2.x/composition/magento/init.sh
+++ magento/2.x/composition/magento/init.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
+set -e
+shopt -s expand_aliases
 
+alias n98="n98-magerun2 --skip-root-check --root-dir=/var/www/html"
+
+echo "Updating xdebug configuration..."
 echo "xdebug.remote_enable=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 echo "xdebug.remote_host=$DOCKER_HOST_IP" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 
-n98-magerun --root-dir=/var/www/html config:set web/unsecure/base_url $BASE_URL
-n98-magerun --root-dir=/var/www/html config:set web/secure/base_url $BASE_URL
+# rm -rf vendor/
+composer install
+rm -rf var/generation/*
+rm -rf var/di/*
+php ./bin/magento module:enable --all
+php ./bin/magento setup:upgrade
+php ./bin/magento setup:di:compile
+php ./bin/magento setup:static-content:deploy
+
+n98 config:set web/unsecure/base_url "http://$DOMAIN/"
+n98 config:set web/secure/base_url "http://$DOMAIN/"
+n98 config:set web/unsecure/base_media_url "http://$DOMAIN/"
+n98 config:set web/secure/base_media_url "http://$DOMAIN/"
+n98 config:delete web/cookie/cookie_domain
+n98 config:set dev/css/merge_css_files 0
+n98 config:set dev/css/minify_files 0
+n98 config:set dev/js/merge_files 0
+n98 config:set dev/js/minify_files 0
+n98 config:delete dev/static/sign
+n98 config:delete catalog/search/engine
+n98 config:delete web/unsecure/base_static_url
+n98 config:delete web/secure/base_static_url
+n98 config:set web/secure/use_in_frontend 0
+n98 config:set web/secure/use_in_adminhtml 0
 
 apache2-foreground
